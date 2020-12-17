1980b186cb376522a9087a7a77d3920ee0cb15cb
wqs <wqs1314xe@gmail.com>
2020-12-17 19:50:26 +0800

    2020.12.17

--- 数组/16.数组中第k个最大的元素.js
+++ 数组/16.数组中第k个最大的元素.js
@@ -1,3 +1,17 @@
+// 215. 数组中的第K个最大元素
+// 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+// 示例 1:
+
+// 输入: [3,2,1,5,6,4] 和 k = 2
+// 输出: 5
+// 示例 2:
+
+// 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+// 输出: 4
+
+// https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+
 const findKthLargest = (nums, k) => {
   const n = nums.length;
 
@@ -49,3 +63,12 @@ function swap (nums, p, q) {
   nums[p] = nums[q];
   nums[q] = temp;
 }
+
+
+let nums = [3, 2, 1, 5, 6, 4]
+let k = 2
+// let res = findKthLargest(nums, k)
+// console.log(res)
+
+let res = findKthLargest(nums, k)
+console.log(res)
